6962c669a1eef6045ec94476f400550531e9c069
BackstagecrewIS <locksmith@backstagecrew.com>
2023-05-01 20:11:10 +0000

    Add delete products

--- products/views.py
+++ products/views.py
@@ -44,9 +44,7 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")  # noqa
                 return redirect(reverse('products'))
 
-            queries = Q(
-                name__icontains=query) | Q(
-                    description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)  # noqa
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -78,9 +76,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')  # noqa
     else:
@@ -116,3 +114,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
